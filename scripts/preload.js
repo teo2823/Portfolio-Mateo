faf7851f36dec6e2f09f699fbe838cd18cc7aa61
Mateo <mateo2823@hotmail.com>
2023-04-24 23:19:44 -0400

    preload

---
+++ scripts/preload.js
@@ -0,0 +1,8 @@
+const loader = document.querySelector('.loader');
+const containerLoader = document.querySelector('.container-loader')
+window.addEventListener('load', () => {
+    loader.style.display = 'none';
+    containerLoader.style.display = 'none';
+});
+
+console.log(loader);
